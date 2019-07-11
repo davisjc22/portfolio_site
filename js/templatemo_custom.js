6db147f8955259c4bad5fe3404294c3b7969e653
Connor Davis <davisjc22@gmail.com>
2019-07-11 10:25:00 -0400

    Add experience page and reformat custom.min.js

--- js/templatemo_custom.js
+++ js/templatemo_custom.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
 		$('a.active').removeClass('active');
     	$(this).addClass('active');
 		$("#menu-container .content").slideUp('slow');
-		$("#menu-container #menu-"+id[1]).slideDown('slow');		
+		$("#menu-container #menu-"+id[1]).slideDown('slow');
 		$("#menu-container .homepage").slideUp('slow');
 		return false;
 	});
@@ -21,6 +21,12 @@ jQuery(document).ready(function($) {
 	$(".main-menu a.aboutbutton").click(function(){
 		$("#menu-container .content").slideUp('slow');
 		$("#menu-container .about-section").slideDown('slow');
+		return false;
+	});
+
+	$(".main-menu a.experiencebutton").click(function(){
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .experience-section").slideDown('slow');
 		return false;
 	});
 
